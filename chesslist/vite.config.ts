8ef139b9f8f31fc3cd0e698e8983a502d45695e8
David <david.moedinger@ketzu.net>
2024-09-17 19:24:06 +0200

    update for pages

--- chesslist/vite.config.ts
+++ chesslist/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "https://ketzu.github.io/chesslist/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
